45667679515a830c0516dd980c113084cb1e1a31
Dominick Rocco <drocco@mmm.com>
2019-08-10 09:05:58 -0500

    Add most basic model predictions

--- services/predict/peer_loan_app.py
+++ services/predict/peer_loan_app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+import os
+import pickle
 
-app = Flask(__name__)
+import flask
+import pandas as pd
+
+
+app = flask.Flask(__name__)
+model_path = os.path.join(os.environ["MODEL_DIR"],
+                          os.environ["MODEL_FILE_NAME"])
+model = pickle.load(open(model_path, 'rb'))
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    data = flask.request.get_json()
+    input_df = pd.DataFrame.from_dict(data)
+    prediction = model.predict_proba(input_df)
+    output = prediction[0]
+    return flask.jsonify(list(output))
 
 
 if __name__ == '__main__':
